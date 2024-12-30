c5dc0ba5fa0ca163dc75c3cbcfbe084fc68d32ac
navkant <navkant.tyagi9@gmail.com>
2024-12-30 09:22:29 +0530

    checking the commits on gh

--- css_js_clock/app.js
+++ css_js_clock/app.js
@@ -9,7 +9,7 @@ function setClockTime() {
     const secondDegrees = (6 * seconds + 270) % 360;
     const minDegrees = (6 * minutes + 270) % 360;
     const hourDegrees = (30 * hours + 270) % 360;
-    console.log(secondDegrees);
+
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
     minHand.style.transform = `rotate(${minDegrees}deg)`;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
